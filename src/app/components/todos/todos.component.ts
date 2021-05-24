d74d38ba0a1c61460d694b44b9e073fd53863359
NguSa1 <sa.nguyen@gmx.de>
2021-05-24 18:49:50 +0200

    finished all basic functions for the app to work

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -14,9 +14,8 @@ export class TodosComponent implements OnInit {
   // title = "I want to inject this string"; --> {{title}}
 
   // this creates an empty array called todos, consisting Todo objects
-  todos:Todo[
-    
-  ];
+  todos:Todo[];
+  inputTodo:string="";
 
   constructor() { }
   // does things on initialisation
@@ -28,11 +27,31 @@ export class TodosComponent implements OnInit {
         completed: false
       },
       {
-        content: "First todo",
+        content: "Second todo",
         completed: false
       }      
     ]
   }
 
+  toggleDone(id:number) {
+    // loop through all the todos and if the i matches with the id it reverses the completed bool
+    this.todos.map((v, i) => {
+      if (i == id) v.completed = !v.completed;
+      return v
+    })
+  }
+
+  deleteTodo(id:number) {
+    this.todos = this.todos.filter((v, i) => i !== id);
+  }
+
+  addTodo() {
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false
+    });
+
+    this.inputTodo = ""
+  }
 }
 
